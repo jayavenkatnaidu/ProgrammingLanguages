bb1321a84cf1193fc73430eed29bb7c26f564942
jayavenkatnaidu <34250030+jayavenkatnaidu@users.noreply.github.com>
2020-05-10 11:40:30 +0530

    Create ControlWhile.java

---
+++ Core Java/ControlWhile.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+
+public class ControlWhile {
+    public static void main(String args[]) {
+        int x=1;
+
+        //while condition to print the numbers from 1-10
+        while(x<=10){
+            System.out.println(x);
+            x++;
+        }
+    }
+}
